54e4de55320ea5869468b81ff9c01ce150c0050a
snehguptafr <guptasneh302004@gmail.com>
2023-11-15 10:06:52 +0530

    add navbar to pages

--- banking-client/src/pages/About.jsx
+++ banking-client/src/pages/About.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function About() {
   return (
     <>
+      <Navbar />
       <h1>About</h1>
     </>
   );
--- banking-client/src/pages/Customers.jsx
+++ banking-client/src/pages/Customers.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Customers() {
   return (
     <>
+      <Navbar />
       <h1>All customers</h1>
     </>
   );
