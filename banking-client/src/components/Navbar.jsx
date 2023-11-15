49f99edff9dc7456eadbfc88855d3f5b8c5934f4
snehguptafr <guptasneh302004@gmail.com>
2023-11-15 10:06:38 +0530

    add navbar

---
+++ banking-client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <span className="nav-heading">GRIP Banking System</span>
+      <div className="nav-links">
+        <span className="nav-pills"><Link to={'/'}>Home</Link></span>
+        <span className="nav-pills"><Link to={'/customers'}>All Customers</Link></span>
+        <span className="nav-pills"><Link to={'/about'}>About</Link></span>
+      </div>
+    </nav>
+  );
+}
