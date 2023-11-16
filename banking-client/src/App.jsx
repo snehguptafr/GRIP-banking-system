96d8f9fb3981d3ca085e3f22df7544ea29b2d1a9
snehguptafr <guptasneh302004@gmail.com>
2023-11-16 12:59:39 +0530

    active page state and dynamic class allotment

--- banking-client/src/App.jsx
+++ banking-client/src/App.jsx
@@ -1,13 +1,17 @@
+import {useState} from "react";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Customers from "./pages/Customers";
 
 export default function App() {
+  const [activePage, setActivePage] = useState("home");
+
   return (
     <>
       <BrowserRouter>
-      
+        <Navbar active={activePage} setActive={setActivePage} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- banking-client/src/components/Navbar.jsx
+++ banking-client/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 // import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import './navbar.css'
 
-export default function Navbar() {
+export default function Navbar({ active, setActive }) {
+  console.log(active)
   return (
     <nav>
       <span className="nav-heading">GRIP Banking System</span>
       <div className="nav-links">
-        <span className="nav-pills"><Link to={'/'}>Home</Link></span>
-        <span className="nav-pills"><Link to={'/customers'}>All Customers</Link></span>
-        <span className="nav-pills"><Link to={'/about'}>About</Link></span>
+        <span className={`nav-pills ${active === 'home' && 'active'}`} onClick={() => setActive('home')}><Link to={'/'}>Home</Link></span>
+        <span className={`nav-pills ${active === 'customers' && 'active'}`} onClick={() => setActive('customers')} ><Link to={'/customers'}>All Customers</Link></span>
+        <span className={`nav-pills ${active === 'about' && 'active'}`} onClick={() => setActive('about')} ><Link to={'/about'}>About</Link></span>
       </div>
     </nav>
   );
