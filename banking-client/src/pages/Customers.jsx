b0562ff3ba698fb43d8249862dfedb6b531ef7f7
snehguptafr <guptasneh302004@gmail.com>
2023-11-16 13:16:18 +0530

    remove duplicates

--- banking-client/src/pages/Customers.jsx
+++ banking-client/src/pages/Customers.jsx
@@ -1,9 +1,7 @@
-import Navbar from "../components/Navbar";
 
 export default function Customers() {
   return (
     <>
-      <Navbar />
       <h1>All customers</h1>
     </>
   );
--- banking-client/src/pages/Home.jsx
+++ banking-client/src/pages/Home.jsx
@@ -1,10 +1,8 @@
-import Navbar from "../components/Navbar";
 
 export default function Home(){
 
     return(
         <>
-        <Navbar />
         <h1>Home</h1>
         </>
     )
