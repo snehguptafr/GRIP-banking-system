a9ab71ed901ef6d25290ea6db57d045265f76ce0
snehguptafr <guptasneh302004@gmail.com>
2023-11-17 12:04:28 +0530

    add footer to page

--- banking-client/src/App.jsx
+++ banking-client/src/App.jsx
@@ -1,22 +1,22 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Customers from "./pages/Customers";
-import "./App.css"
+import "./App.css";
 
 export default function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Navbar  />
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/customers" element={<Customers />} />
         </Routes>
-
+      <Footer />
       </BrowserRouter>
     </>
   );
