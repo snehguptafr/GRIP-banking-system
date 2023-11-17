a4a47736f6d205d1edf8bf4360833778334e91f7
snehguptafr <guptasneh302004@gmail.com>
2023-11-17 12:28:55 +0530

    add dummy table

--- banking-client/src/pages/Customers.jsx
+++ banking-client/src/pages/Customers.jsx
@@ -1,8 +1,34 @@
+import "./customers.css"
 
 export default function Customers() {
   return (
-    <>
+    <main className="customers">
       <h1>All customers</h1>
-    </>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Account No.</th>
+            <th>Name</th>
+            <th>E-mail</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>001</td>
+            <td>John Doe</td>
+            <td>john@example.com</td>
+            <td>$1,000.00</td>
+          </tr>
+          <tr>
+            <td>002</td>
+            <td>Jane Doe</td>
+            <td>jane@example.com</td>
+            <td>$2,500.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
   );
 }
