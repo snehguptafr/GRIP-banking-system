c4defe3f44ed173223ad567c5c118e378b1f1e7e
snehguptafr <guptasneh302004@gmail.com>
2023-11-17 10:47:19 +0530

    add content

--- banking-client/src/pages/Home.jsx
+++ banking-client/src/pages/Home.jsx
@@ -1,9 +1,34 @@
+import "./home.css";
+import { Link } from "react-router-dom";
 
-export default function Home(){
-
-    return(
-        <>
-        <h1>Home</h1>
-        </>
-    )
+export default function Home() {
+  return (
+    <main className="home">
+      <h1>Home</h1>
+      <p>
+        This is a mockup of a basic banking system that imitates transfer of
+        money from one account to another. The tech stack used in building this
+        project was MERN.
+      </p>
+      <p>
+        Head over to <Link to={"/customers"}>All Customers</Link> to try it out.
+      </p>
+      <p>
+        The main objective behind building this project was to learn and setup a
+        remote Mongo database, connect it to a Node server to read
+        and modify data and present it on client-side.
+      </p>
+      <p>
+        This is project was a task under Graduate Rotational Internship Program
+        by <a href="http://thesparksfoundation.sg/">The Sparks Foundation</a>.
+        <br />
+      </p>
+      <p className="intern-link">
+        Apply for internship:{" "}
+        <a href="https://internship.thesparksfoundation.info/">
+          internship.thesparksfoundation.info
+        </a>
+      </p>
+    </main>
+  );
 }
